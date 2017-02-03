fc19b85d18186a47fc16f709a8ea5c2497c2850c
Osmael de Sousa Braga <osmaelsousa.ti@gmail.com>
2017-02-03 01:14:13 -0300

    Creating new activities

--- app/src/main/java/osmael/example/com/miwok/FamilyActivity.java
+++ app/src/main/java/osmael/example/com/miwok/FamilyActivity.java
@@ -3,12 +3,11 @@ package osmael.example.com.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_family);
     }
-    
 }
