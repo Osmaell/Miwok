502c88b7b5bd353f9b52a1e926b6130b12ffa446
Osmael de Sousa Braga <osmaelsousa.ti@gmail.com>
2017-02-08 15:48:16 -0300

    Show view in the activity_numbers layout

--- app/src/main/java/osmael/example/com/miwok/NumbersActivity.java
+++ app/src/main/java/osmael/example/com/miwok/NumbersActivity.java
@@ -1,7 +1,11 @@
 package osmael.example.com.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,6 +13,27 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<>();
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.activity_numbers);
+
+        for(int i = 0; i < words.size(); i++) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(i));
+            rootView.addView(wordView);
+        }
+
     }
 
 }
